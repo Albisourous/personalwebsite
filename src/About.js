6a8e6f8d2cf16c6359b7fb75d3252b0b07a5f615
Albin Shrestha <shrestha_albin@yahoo.com>
2021-12-31 16:24:46 -0600

    first

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+import "./styles/App.css";
+
+export const About = () => {
+    return (
+
+        <div className="About">
+        </div >
+    );
+};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import React, { useEffect } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { vres } from "./mixin";
+
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+const GlobalStyle = createGlobalStyle`
+*,
+*::after,
+*::before {
+  margin: auto;
+  padding: 0;
+  box-sizing: border-box;
+  user-select: none;
+  transform-style: preserve-3d;
+  -webkit-tap-highlight-color: transparent;
+}
+
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: ${(props) => vres(props.isChrome ? 100 : 10)};
+  height: 100vh;
+  overflow: hidden;
+  background-image: radial-gradient(white, #2a9aea);
+  transform: translateZ(0);
+}
+`;
+
+const SafariWarning = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  padding: 16px;
+  text-align: center;
+  color: white;
+  font-weight: bold;
+  background-color: #e6804d;
+`;
 
 function App() {
+  useEffect(() => {
+    const isBrave = navigator.brave !== undefined;
+    isBrave && document.body.classList.add("brave");
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isSafari && (
+        <SafariWarning>
+          Currently Safari doesn't fully support this site. Please switch to
+          Chrome for better viewing experience.
+        </SafariWarning>
+      )}
+      <GlobalStyle isChrome={isChrome} />
     </div>
   );
 }
